9c8858ceab6489e42511938d3e08fc174ad5fc63
alexNMD <ale.normand@icloud.com>
2024-02-26 11:27:21 +0100

    Fixed app running in production

--- __init__.py
+++ __init__.py
@@ -8,10 +8,12 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/<string:project>")
 def home(project=None):
+    local_path = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+    projects_path = f"{ local_path }/static/img/projects"
     project = project.lower() if project else ""
-    projects_availables = [p.lower() for p in os.listdir("static/img/projects")] or []
+    projects_availables = [p.lower() for p in os.listdir(projects_path)] or []
     project_selected = project if project in projects_availables else projects_availables[0]
-    images_availables = os.listdir(f"static/img/projects/{ project_selected }") or []
+    images_availables = os.listdir(f"{ local_path }/static/img/projects/{ project_selected }") or []
 
     return render_template("index.html",
                            projects=projects_availables,
@@ -21,3 +23,6 @@ def home(project=None):
 @app.errorhandler(404)
 def page_not_found(e):
     return redirect('/'), 302
+
+if __name__ == "__main__":
+    app.run(debug=True)
