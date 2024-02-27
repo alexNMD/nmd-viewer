78411be9b37b7d9f12cd69e81a79668c87715732
alexNMD <ale.normand@icloud.com>
2024-02-28 00:06:10 +0100

    optim

--- __init__.py
+++ __init__.py
@@ -5,21 +5,19 @@ from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
+projects_path = "C:/Users/user/Documents/DEVs/nmd_viewer/static/img/projects"
 
 @app.route("/")
 @app.route("/<string:project>")
 def home(project=None):
-    local_path = os.path.join(os.path.dirname(os.path.realpath(__file__)))
-    projects_path = f"{ local_path }/static/img/projects"
     project = project.lower() if project else ""
     projects_lst = [p.lower() for p in os.listdir(projects_path)] or []
     project_selected = project if project in projects_lst else projects_lst[0]
-    images_lst = os.listdir(f"{ local_path }/static/img/projects/{ project_selected }") or []
 
-    regex_align = '\$(.*?)\$'
-    images_lst_dct = [{ "name": i, "mobile_align": f"{re.search(regex_align, i).group(1)}%" if re.search(regex_align, i) else None } for i in images_lst]
+    images_lst = os.listdir(f"{ projects_path }/{ project_selected }") or []
 
-    print(images_lst_dct)
+    rgx = '\$(.*?)\$' # regex searched
+    images_lst_dct = [{ "name": i, "mobile_align": f"{re.search(rgx, i).group(1)}%" if re.search(rgx, i) else None } for i in images_lst]
 
     return render_template("index.html",
                            projects=projects_lst,
