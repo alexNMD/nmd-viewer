697ba712729f9ea5c8a2f1ee41bb4dcfeab6237d
Alexandre <alexandre@raspberrypi>
2024-06-04 22:28:59 +0200

    added debug mode for dev

--- app.py
+++ app.py
@@ -33,4 +33,5 @@ def page_not_found(e):
     return redirect('/'), 302
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000, host='0.0.0.0')
