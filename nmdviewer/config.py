720e1f17a1393767330ee71bee59ce7cc62fba32
alexandre <alexandre.normand@cinego.net>
2026-02-10 10:03:08 +0100

    remove instagram link

--- nmdviewer/config.py
+++ nmdviewer/config.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DNS = os.getenv("NMDNS")
-INSTAGRAM_URL = os.getenv("INSTAGRAM_PROFILE")
 GITHUB_URL = os.getenv("GITHUB_PROFILE")
 
 PROJECTS_PATH = os.getenv("PROJECTS_PATH", default="/app/nmd_project")
